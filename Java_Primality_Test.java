8f722cc3a0a54a4d66b63a673be4b8be3d3b6a12
Renu Sree <renusree0704@gmail.com>
2024-11-10 22:28:07 +0530

    Add files via upload

---
+++ Java_Primality_Test.java
@@ -0,0 +1,32 @@
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Java_Primality_Test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        ;
+        
+        String n=br.readLine();
+        br.close();
+        BigInteger bi=new BigInteger(n);
+        if(bi.isProbablePrime(10)){
+            System.out.println("prime");
+        }else{
+                System.out.println("not prime");
+            }
+        
+
+        
+    }
+    }
+
+
