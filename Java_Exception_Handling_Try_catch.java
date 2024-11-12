2f1ff88ccc962ccbd4375424743dc31b400c3d77
Renu Sree <renusree0704@gmail.com>
2024-11-12 22:45:12 +0530

    firstcommit

---
+++ Java_Exception_Handling_Try_catch.java
@@ -0,0 +1,24 @@
+package oops;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Java_Exception_Handling_Try_catch {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x, y;
+
+        try {
+            x = sc.nextInt();
+            y = sc.nextInt();
+            System.out.println(x / y);
+        } catch (InputMismatchException e) {
+            System.out.println("java.util.InputMismatchException");
+        } catch (ArithmeticException e) {
+            System.out.println("java.lang.ArithmeticException: / by zero");
+        }
+    }
+}
